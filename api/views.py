8d70868970c2929b204b6c16f7ef5179e5c17764
danydurand <danydurand@gmail.com>
2022-08-06 14:07:29 -0400

    Login Endpoint
    - login endpoint created and tested

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.authtoken.models import Token
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
 
 
 class TodoListCreate(generics.ListCreateAPIView):
@@ -47,20 +48,35 @@ class TodoToggleComplete(generics.UpdateAPIView):
 
 @csrf_exempt
 def signup(request):
-    print('getting to signup..')
     if request.method == 'POST':
-        print('It is a POST request')
         try:
             data = JSONParser().parse(request)
-            print('data is: ',data)
             user = User.objects.create_user(
                 username=data['username'],
                 password=data['password'],
             )
             user.save()
-            print('User created')
             token = Token.objects.create(user=user)
-            print('Token',token)
             return JsonResponse({'token':str(token)},status=201)
         except IntegrityError:
             return JsonResponse({'error':'username taken. choose another username'},status=400)
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(
+            request, 
+            username=data['username'],
+            password=data['password'],
+        )
+        if user is None:
+            return JsonResponse({'error':'unable to login. check username and password'},status=400)
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)},status=201)
+
